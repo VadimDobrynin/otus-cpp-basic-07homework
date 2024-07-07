4754cdbedd3c0815bb37e410e0ae324bb24b1869
vadim_dobrunin <vadim_dobrunin@mail.ru>
2024-07-07 15:49:31 +0300

    что то стало ближе к успеху

---
+++ MyTests/TestVector.cpp
@@ -0,0 +1,8 @@
+//
+// Created by vadim on 03.07.2024.
+//
+#include <iostream>
+int main(){
+    std::cout << "Hello from tests main" << std::endl;
+    return 0;
+}
--- containers/main.cpp
+++ containers/main.cpp
@@ -1,6 +1,8 @@
 //
 // Created by vadim on 20.06.2024.
 //
+#include <iostream>
 int main(){
+    std::cout << "Hello from containers main" << std::endl;
     return 0;
 }
